d6216c1b36981c98492711c7caf02e9aee6605d1
Ashish Raj <ashi1110000111@gmail.com>
2024-06-01 10:22:58 +0530

    update-chatbot

--- agency/src/ChatBot/tawkToWidget.js
+++ agency/src/ChatBot/tawkToWidget.js
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const TawkToWidget = () => {
   useEffect(() => {
-    const s = document.createElement('script');
-    s.type = 'text/javascript';
+    const tawkToSrc = process.env.REACT_APP_TAWKTO_SRC;
+    if (!tawkToSrc) {
+      console.error(
+        "TawkTo script source is not defined in the environment variables"
+      );
+      return;
+    }
+
+    const s = document.createElement("script");
+    s.type = "text/javascript";
     s.async = true;
-    s.src = 'https://embed.tawk.to/65c234390ff6374032c9ef70/1hlv9o0ko';
-    s.charset = 'UTF-8';
-    s.setAttribute('crossorigin', '*');
+    s.src = tawkToSrc;
+    s.charset = "UTF-8";
+    s.setAttribute("crossorigin", "*");
     document.head.appendChild(s);
 
     return () => {
