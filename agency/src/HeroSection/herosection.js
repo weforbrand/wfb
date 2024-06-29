27be7eb2e996ce3656650abc614a039214d0d4ce
Ashish Raj <ashi1110000111@gmail.com>
2024-06-29 12:03:56 +0530

    update-hero-section

--- agency/src/HeroSection/herosection.js
+++ agency/src/HeroSection/herosection.js
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./herosection.css";
 
+const services = ["stand out", "lead the way", "unforgettable"];
+
 const HeroSection = () => {
+  const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentServiceIndex((prevIndex) => (prevIndex + 1) % services.length);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="hero-sec-main">
       <div className="hero-overlay">
         <div className="hero-text">
           <div>
-            <h1>
-              Empowering Transformation: The Architects of Digital Disruption
+            <h1 style={{ marginTop: "151px", fontSize: "55px" }}>
+              Bringing Your Brand to Life
             </h1>
+
+            <h2 className="no-wrap">
+              We make your brand{" "}
+              <span
+                className="zoom-out"
+                style={{
+                  color: "#ffca00",
+                  paddingLeft: "20px",
+                  paddingRight: "20px",
+                }}
+              >
+                {services[currentServiceIndex]}
+              </span>{" "}
+            </h2>
           </div>
+
           <p>
-            <strong>
-              Transform Your Business with our Innovative Marketing Services
-            </strong>
-          </p>
-          <p>
-            At WeForBrands, we drive business growth by crafting digital
-            experiences that navigate complex challenges with creativity,
-            innovation, and agility. Our solutions are designed to turn
-            obstacles into opportunities, businesses into brands.
+            Make your brand the one everyone talks about. At Weforbrands, we
+            specialize in making brands lovable and visible. Engage with us, and
+            let's make your brand a beloved one in its industry.
           </p>
           <div className="hero-button">
             <a href="/contact">
-              <button>Contact Our Experts</button>
+              <button>Let's Talk</button>
             </a>
           </div>
         </div>
